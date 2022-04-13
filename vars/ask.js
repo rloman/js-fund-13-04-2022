c0154252b1ccb1229586f31bbc18c7b3d45a50ed
Raymond Loman <raymond@carpago.nl>
2022-04-13 13:38:40 +0200

    Move vars to upperdir

---
+++ vars/ask.js
@@ -0,0 +1,12 @@
+let aantal = parseInt(window.prompt("Geef aantal"));
+
+let prijs = parseFloat(window.prompt("Geef prijs"));
+console.log("Prijs afgerond is: "+prijs);
+
+let totaalPrijs = (aantal * prijs).toFixed(2);
+
+// korting ...
+
+alert("Totaalprijs: "+totaalPrijs);
+
+console.log(55 * 8.44);
