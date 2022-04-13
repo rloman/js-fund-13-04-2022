1af266fdb8f63a9982ba5047d0de02f7ac67c82a
Raymond Loman <raymond@carpago.nl>
2022-04-13 21:32:21 +0200

    Add new features: spread operator, power with ** and generator function

---
+++ new_features/generatorfunction.js
@@ -0,0 +1,15 @@
+function* generateEvenNumbers(start, end) {
+    let base = Math.floor(start / 2) * 2; // :-)
+
+    while (base < end) {
+        yield base += 2;
+    }
+}
+
+const gen = generateEvenNumbers(5, 26);
+
+for (let value = gen.next().value; value != undefined; value = gen.next().value) {
+    console.log(value);
+}
+
+
---
+++ new_features/machtsverheffen.js
@@ -0,0 +1,5 @@
+// nieuwe operator, voor machtsverheffen
+
+let result = 2 ** 3; // 2 tot de 3e macht ::= 3;
+
+console.log(result);
