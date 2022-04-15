81d0a207207e71e1c10b96d2471440f6d15393d0
Raymond Loman <raymond@carpago.nl>
2022-04-15 10:09:30 +0200

    Add more events

--- events/basic/solution/lib.js
+++ events/basic/solution/lib.js
@@ -3,10 +3,9 @@ function buttonClicked() {
     element.innerHTML="This is the replacement :-)";
 }
 
-function setColor(event) {
+function setColor() {
     let element = document.getElementById("content");
     let color = document.getElementById("color").value;
     element.style.backgroundColor=color;
 
-    alert(event);
 }
