a997b45b876bb5d2f699aa5722e5176c2647d9f6
Raymond Loman <raymond@carpago.nl>
2022-04-15 16:21:19 +0200

    Add demo code for break and continue

--- controlestructuren/loops/main.js
+++ controlestructuren/loops/main.js
@@ -50,6 +50,30 @@ for (let cijfer = 0; cijfer < 10; cijfer = cijfer + 1) {
     console.log("For loop, value of i: " + cijfer);
 }
 
-for(let cijfer = 10;cijfer > 0;cijfer = cijfer + 1) {
+for(let cijfer = 10;cijfer > 0;cijfer = cijfer -1 ) {
     console.log("For loop, value of i: " + cijfer);
 }
+
+
+
+// demo continue
+for(let i = 1;i<=20;i++) {
+    if(i == 13) {
+        continue; // takeaway::=continue slaat de huidige stap over!!!
+    }
+    console.log("continue: "+i);
+}
+
+// demo break
+for(let i = 1;i<=20;i++) {
+    if(i == 13) {
+        break; // takeaway::=continue slaat de huidige stap over!!!
+    }
+    console.log("break: "+i);
+}
+// hier gaat ie verder na de break;
+
+// Exercise
+// Maak een loop van 15 tot 0 voor alle oneven getallen.
+// Als de waarde is gelijk aan: 11 dan pas je continue toe.
+
