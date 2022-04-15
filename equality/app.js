2d865d17c36bb1f5ca4ddf8ae8f57953d6411902
Raymond Loman <raymond@carpago.nl>
2022-04-15 15:24:35 +0200

    Add equality

---
+++ equality/app.js
@@ -0,0 +1,9 @@
+// == , ===
+// == betekent: semantisch gelijk, d.w.z. "3" is gelijk aan 3
+// === betekent, streng gelijk: d.w.z. "3" is "3" maar "3" is niet gelijk aan 3
+
+console.log("3" == 3);
+console.log("3" !== 3);
+console.log("4" != 3);
+console.log("drie" != 3);
+
